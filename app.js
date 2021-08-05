1fa3d9f7b42c88b86bfa684799caf31b6934721e
Alper KILIÇKAYA <alperkilickaya@gmail.com>
2021-08-05 17:48:13 +0300

    sweatalert set to default

--- app.js
+++ app.js
@@ -2,47 +2,43 @@ const loginForm = document.getElementById("registerForm"); // formu yakala
 let email = document.getElementsByName("email")[0]; // email alanını yakala
 
 
-loginForm.addEventListener("submit", function(e){ // formun std submit olma durumunu engelle
-    e.preventDefault();
-})
+let swal= { //swetalert default ayarı.
+    title: 'Uyarı!',
+    html: 'Lütfen Boş Alan Bırakmayınız!',
+    icon: 'warning',
+    confirmButtonText: 'Tamam'
+}
 
 function gonder(){ // login butonuna basılınca çalışan fonksiyon
-    let password = document.getElementsByName("password")[0];// şifre alanı
 
+    loginForm.addEventListener("submit", function(e){ // formun std submit olma durumunu engelle
+        e.preventDefault();
+    })
+
+    let password = document.getElementsByName("password")[0];// şifre alanı
     const regexPattern = /\S+\@+\S+\.+\S{3}/ // regex paternim. 
 
     if((email.value.trim() == "") || (password.value.trim() == "" ) ){ // alanlar boş olamaz
-        Swal.fire({
-            title: 'Uyarı!',
-            html: 'Lütfen Boş Alan Bırakmayınız!',
-            icon: 'warning',
-            confirmButtonText: 'Tamam'
-        
-        });
+
+        Swal.fire(swal);
+
     }else if (!regexPattern.test(email.value.trim())) { // email alanına girilen değer regex ile karşılaştır.
-        Swal.fire({
-            title: 'Uyarı!',
-            html: 'Email Hatalı',
-            icon: 'warning',
-            confirmButtonText: 'Tamam'
+
+        swal.html = 'Email Hatalı'
+        Swal.fire(swal);
         
-        });
     }else if (password.value.trim().length < 6) { // şifre 6 karakterden az ise
-        Swal.fire({
-            title: 'Uyarı!',
-            html: 'Şifre 6 Karakterden Az Olamaz!',
-            icon: 'warning',
-            confirmButtonText: 'Tamam'
-        
-        });
+
+        swal.html = 'Şifre 6 Karakterden Az Olamaz!'
+        Swal.fire(swal);
+
      }else{ // koşullar uygunsa sayfa yönlendir.
-        Swal.fire({
-            title: 'Başarılı',
-            html: 'Sayfa Yenilenecek!',
-            icon: 'success',
-            confirmButtonText: 'Tamam'
-        
-        }).then(function(){ // tamam butonu sonrası yönlendirmeyi yap.
+
+        swal.title = 'Başarılı'    
+        swal.html = 'Sayfa Yenilenecek!';
+        swal.icon = 'success'
+
+        Swal.fire(swal).then(function(){ // tamam butonu sonrası yönlendirmeyi yap.
             location.replace("index.html")
         });
         
